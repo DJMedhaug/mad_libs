795c43f1848efc30d923834b552f8aa589bfd741
Dana <djmedhaug@gmail.com>
2015-09-17 10:40:26 -0700

    true

--- madlib.py
+++ madlib.py
@@ -1,1 +1,11 @@
-Yo...
+animal = input('Enter an animal ')
+madlib = animal
+madlib += ' can be found running wild in '
+madlib += input(' Enter a city... ')
+madlib += ' causing distruction at every turn, when he turned '
+madlib += input('Enter a color ')
+madlib += ' he gained strength and speed, {} tried to run from the people. '.format(animal)	
+madlib += 'When he changed color the people ran the other way and jumped into a '
+madlib += input('Enter a car ')
+madlib += ' and took off, from there he was free to do as he pleased. '
+print (madlib)
